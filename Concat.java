df638be646a9193ef47bf30493372f2d0012b65a
Priti <gpriti2580@gmail.com>
2023-11-08 13:45:15 +0530

    Concat two objects

---
+++ Concat.java
@@ -0,0 +1,18 @@
+public class Concat {
+    int method1()
+    {
+        System.out.println("Method 1 called");
+        return 100;
+    }
+    int method2()
+    {
+        System.out.println("method 2 called");
+        return 400;
+    }
+    public static void main(String args[])
+    {
+        System.out.println(new Concat().method1()+new Concat().method2());
+    }
+
+}
+
